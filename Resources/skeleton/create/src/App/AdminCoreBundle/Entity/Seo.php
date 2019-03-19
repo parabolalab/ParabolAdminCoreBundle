1158e07558f5efc84a80b357909ee10ed048d1b5
Marcin Kalota <marcin.kalota@aliso.pl>
2019-03-19 11:09:57 +0100

    seo more flexible structure

--- Resources/skeleton/create/src/App/AdminCoreBundle/Entity/Seo.php
+++ Resources/skeleton/create/src/App/AdminCoreBundle/Entity/Seo.php
@@ -3,12 +3,14 @@
 namespace App\AdminCoreBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * Seo
  *
  * @ORM\Table(name="parabol_seo")
  * @ORM\Entity(repositoryClass="Parabol\AdminCoreBundle\Repository\SeoRepository")
+ * @UniqueEntity(fields={"url", "inherited"}, errorPath="url")
  */
 class Seo extends \Parabol\AdminCoreBundle\Model\Seo
 {
